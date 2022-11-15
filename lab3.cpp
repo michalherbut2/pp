6e4ea6ae18bfc1e0b68be28127925b837da0731a
Michal <michal@michalarch.local>
2022-11-15 18:24:07 +0100

    Changed menu

--- lab3.cpp
+++ lab3.cpp
@@ -2,7 +2,7 @@
 Autor:  Michał Herbut
 Grupa:  WT/N 11:15     (Wtorek nieparzysty godz 11:15)
 Temat:  Program z laboratorium 3
-Data:   14 listopada 2022 r.
+Data:   15 listopada 2022 r.
 */
 
 #include <stdio.h>
@@ -12,23 +12,31 @@ Data:   14 listopada 2022 r.
 
 #define ROZMIAR 5
 
-using namespace std;
-
 void readArray(float array[]) {
+  printf("\n--------------------------------\n\n");
+  printf("Funkcja wczytuje elementy tablicy.\n");
+
   for (int i = 0; i < ROZMIAR; i++) {
     printf("Tab[%d] = ", i);
     scanf("%f", &array[i]);
   }
+  printf("\n");
 }
 
 void printArray(float array[]) {
+  printf("\n--------------------------------\n\n");
+  printf("Funkcja drukuje elementy tablicy.\n");
+
   printf("\nTab = [");
   for (int i = 0; i < ROZMIAR; i++)
     printf("  %2.1f", array[i]);
   printf("  ]\n\n");
 }
 
-void avgArray(float array[]) {
+void printArraySumAndAvg(float array[]) {
+  printf("\n--------------------------------\n\n");
+  printf("Funkcja drukuje liczbe, sume i srednia dodatnich i ujemnych liczb w tablicy.\n");
+
   int numNegative = 0, numPositive = 0;
   float sumNegative = 0, sumPositive = 0, avgNegative, avgPositive;
 
@@ -59,7 +67,10 @@ void avgArray(float array[]) {
     printf("W tablicy nie ma liczb ujemnych.\n\n");
 }
 
-void infoArray(float array[]) {
+void printArrayOrder(float array[]) {
+  printf("\n--------------------------------\n\n");
+  printf("Funkcja drukuje informacje o uporzadkowaniu elementow tablicy.\n");
+
   int asc = 0, desc = 0, constant = 0;
 
   for (int i = 0; i < ROZMIAR - 1; i++) {
@@ -67,25 +78,18 @@ void infoArray(float array[]) {
     if (array[i] > array[i+1]) desc++;
     if (array[i] < array[i+1]) asc++;
   }
-  if (asc == ROZMIAR - 1) printf("elementy tablicy sa uporzadkowane rosnaco\n");
-  else if (desc == ROZMIAR - 1) printf("elementy tablicy sa uporzadkowane malejaco\n");
-  else if (constant == ROZMIAR - 1) printf("elementy tablicy maja jednakowa wartosc\n");
-  else if (desc && constant && !asc) printf("elementy tablicy sa uporzadkowane nierosnaco\n");
-  else if (asc && constant && !desc) printf("elementy tablicy sa uporzadkowane niemalejaco\n");
-  else if (asc && desc) printf("elementy tablicy sa nieuporzadkowane\n");
+  if (asc == ROZMIAR - 1) printf("elementy tablicy sa uporzadkowane rosnaco\n\n");
+  else if (desc == ROZMIAR - 1) printf("elementy tablicy sa uporzadkowane malejaco\n\n");
+  else if (constant == ROZMIAR - 1) printf("elementy tablicy maja jednakowa wartosc\n\n");
+  else if (desc && constant && !asc) printf("elementy tablicy sa uporzadkowane nierosnaco\n\n");
+  else if (asc && constant && !desc) printf("elementy tablicy sa uporzadkowane niemalejaco\n\n");
+  else if (asc && desc) printf("elementy tablicy sa nieuporzadkowane\n\n");
 }
 
-void task1(float array[]) {
+void fillRandomArray(float array[]) {
   printf("\n--------------------------------\n\n");
-  printf("Program drukuje wczytane elementy tablicy oraz liczbe, sume i srednia elementow ujemnych i dodatnich oraz ich uporzadkowanie.\n");
+  printf("Funkcja wypelnia tablice liczbami losowymi z zadanego przedzialu.\n");
 
-  readArray(array);
-  printArray(array);
-  avgArray(array);
-  infoArray(array);
-}
-
-void randomArray(float array[]) {
   float min, max;
   printf("Podaj min: ");
   scanf("%f", &min);
@@ -94,19 +98,13 @@ void randomArray(float array[]) {
   srand(time(NULL));
   for (int i = 0; i < ROZMIAR; i++)
     array[i] = min + (max - min) * rand() / RAND_MAX ;
-}
-
-void task2(float array[]) {
-  printf("\n--------------------------------\n\n");
-  printf("Program drukuje z podanego przedzialu losowe elementy tablicy oraz liczbe, sume i srednia elementow dodatnich i ujemnych oraz ich uporzadkowanie.\n");
-
-  randomArray(array);
-  printArray(array);
-  avgArray(array);
-  infoArray(array);
+  printf("\n");
 }
 
 void bubbleSort(float array[]) {
+  printf("\n--------------------------------\n\n");
+  printf("Funkcja sortuje elementy tablicy rosnaco.\n\n");
+
   for (int i = 0; i < ROZMIAR - 1; i++)
     for (int j = 0; j < ROZMIAR - i - 1; j++)
       if (array[j] > array[j+1]) {
@@ -116,17 +114,6 @@ void bubbleSort(float array[]) {
       }
 }
 
-void task3(float array[]) {
-  printf("\n--------------------------------\n\n");
-  printf("Program drukuje z podanego przedzialu posortowane rosnaco, losowe elementy tablicy oraz liczbe, sume i srednia elementow dodatnich i ujemnych oraz ich uporzadkowanie.\n");
-
-  randomArray(array);
-  bubbleSort(array);
-  printArray(array);
-  avgArray(array);
-  infoArray(array);
-}
-
 void printAlphabetArray(char alphabetArray[]) {
   for (int i = 0; i < 26; i++) {
     printf("litera %c %d ", 'A'+i, alphabetArray[i]);
@@ -134,6 +121,7 @@ void printAlphabetArray(char alphabetArray[]) {
       printf("#");
     printf("\n");
   }
+  printf("\n");
 }
 
 void task4() {
@@ -154,38 +142,39 @@ void task4() {
   if (letterCounter > 0) {
     printf("\n\nWpisano ogolem %d znakow w tym:\n", letterCounter);
     printAlphabetArray(alphabetArray);  
-  } else
-    printf("\n\nNic nie wpisano!\n");
+  } else 
+    printf("\n\nNic nie wpisano!\n\n");
 }
 
 int main() {
-  float array[ROZMIAR];
+  float array[ROZMIAR]={};
   int programNumber;
   do {
     printf("---------------------------------\n\n");
     printf("Autor: Michal Herbut (WT/N 11:15) \n\n");
-    printf("1 | operacje na tablicy\n");
-    printf("2 | operacje na losowej tablicy\n");
-    printf("3 | sortowanie\n");
-    printf("4 | licznik liter\n\n");
-    printf("0 | zamknij program\n\n");
+    printf("1 | Wczytaj tablice\n");
+    printf("2 | Wypisz tablice\n");
+    printf("3 | Wypisz liczbe, sume i srednia\n");
+    printf("4 | Wypisz uporzadkowanie\n");
+    printf("5 | Losuj tablice\n");
+    printf("6 | Sortuj tablice\n");
+    printf("7 | Licznik liter (Zadanie 4)\n\n");
+    printf("0 | Zamknij program\n\n");
     printf("Jaki program? ");
 
 		scanf("%d", &programNumber);
-		switch(programNumber){
-      case 1:
-        task1(array); break;
-      case 2:
-        task2(array); break;
-      case 3:
-        task3(array); break;
-      case 4:
-        task4(); break;
-      case 0:
-        break;
+		switch (programNumber){
+      case 1: readArray(array); break;
+      case 2: printArray(array); break;
+      case 3: printArraySumAndAvg(array); break;
+      case 4: printArrayOrder(array); break;
+      case 5: fillRandomArray(array); break;
+      case 6: bubbleSort(array); break;
+      case 7: task4(); break;
+      case 0: break;
       default:
         printf("\n--------------------------------\n");
-        printf("\nPODAJ NUMER OD 0 Do 4!!!\n\n");
+        printf("\nPODAJ NUMER OD 0 DO 7!!!\n\n");
         break;
 		}
 	} while(programNumber);
